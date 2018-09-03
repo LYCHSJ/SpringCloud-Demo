9a9215adfb63fd3809d0ad9a27a6b5db3a84c41c
LYCHSJ <576094432@qq.com>
2018-09-03 11:13:17 +0800

    bugfix in RestTemplate

--- RestTemplate/consumer/src/main/java/com/alex/springcloud/resttemplate/consumer/controller/ConsumerBookController.java
+++ RestTemplate/consumer/src/main/java/com/alex/springcloud/resttemplate/consumer/controller/ConsumerBookController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -14,6 +15,7 @@ import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
+@RestController
 public class ConsumerBookController {
 
     @Autowired
