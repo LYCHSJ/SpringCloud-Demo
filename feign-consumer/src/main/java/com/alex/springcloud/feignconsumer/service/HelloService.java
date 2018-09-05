eab16b078f15ab3757fffa2c75c2a44ca4c31f70
LYCHSJ <576094432@qq.com>
2018-09-05 20:15:26 +0800

    Feign Consumer Implements

--- feign-consumer/src/main/java/com/alex/springcloud/feignconsumer/service/HelloService.java
+++ feign-consumer/src/main/java/com/alex/springcloud/feignconsumer/service/HelloService.java
@@ -1,10 +1,22 @@
 package com.alex.springcloud.feignconsumer.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.print.Book;
 
 @FeignClient("hello-service")
 public interface HelloService {
     @RequestMapping("/hello")
     String hello();
+
+    @RequestMapping(value = "/hello_with_name", method = RequestMethod.GET)
+    String hello(@RequestParam String name);
+
+    @RequestMapping(value = "/hello_in_header", method = RequestMethod.GET)
+    Book hello(@RequestHeader("name") String name, @RequestHeader("author") String author,
+               @RequestHeader("price") Integer price);
+
+    @RequestMapping(value = "/hello_in_body", method = RequestMethod.POST)
+    String hello(@RequestBody Book book);
 }
