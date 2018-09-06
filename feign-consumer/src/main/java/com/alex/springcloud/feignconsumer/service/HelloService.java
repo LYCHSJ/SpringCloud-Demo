271350666985d1661c80fcaa31361485e3449af2
LYCHSJ <576094432@qq.com>
2018-09-06 20:03:52 +0800

    zuul-api-gateway first commit

--- feign-consumer/src/main/java/com/alex/springcloud/feignconsumer/service/HelloService.java
+++ feign-consumer/src/main/java/com/alex/springcloud/feignconsumer/service/HelloService.java
@@ -1,9 +1,9 @@
 package com.alex.springcloud.feignconsumer.service;
 
+import com.alex.springcloud.RestTemplate.entity.Book;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.print.Book;
 
 @FeignClient("hello-service")
 public interface HelloService {
@@ -11,11 +11,11 @@ public interface HelloService {
     String hello();
 
     @RequestMapping(value = "/hello_with_name", method = RequestMethod.GET)
-    String hello(@RequestParam String name);
+    String hello(@RequestParam("name") String name);
 
     @RequestMapping(value = "/hello_in_header", method = RequestMethod.GET)
     Book hello(@RequestHeader("name") String name, @RequestHeader("author") String author,
-               @RequestHeader("price") Integer price);
+               @RequestHeader("price") Integer price, @RequestHeader("publisher") String publisher);
 
     @RequestMapping(value = "/hello_in_body", method = RequestMethod.POST)
     String hello(@RequestBody Book book);
---
+++ zuul-api-gateway/src/main/java/com/alex/springcloud/zuulapigateway/ZuulApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.alex.springcloud.zuulapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApiGatewayApplication.class, args);
+    }
+}
